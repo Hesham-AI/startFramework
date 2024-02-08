909455ef8cc07865ca0ba9daf8a2c35477a24bb5
hesham <152308429+Hesham-AI@users.noreply.github.com>
2024-02-08 20:53:51 +0200

    upload

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.css']
+})
+export class PortfolioComponent {
+
+
+
+  Images:any[]=[
+{img:'../../assets/poert1.png'},
+{img:'../../assets/port2.png'},
+{img:'../../assets/port3.png'},
+{img:'../../assets/poert1.png'},
+{img:'../../assets/port2.png'},
+{img:'../../assets/port3.png'},
+  ]
+
+
+
+
+}
